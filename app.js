df929c74eaca7f5bba29969ed4dca511474c2d56
Robin <robin.rh656@gmail.com>
2023-09-19 17:04:57 +0600

    first

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello world')
+})
+
+app.listen(3000, () => {
+    console.log('App is runing')
+})
